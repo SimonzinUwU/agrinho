76e6b30ca965a2a6842c1c4bb6ce78acd6e1125a
SimonzinUwU <110109892+SimonzinUwU@users.noreply.github.com>
2024-08-05 11:16:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('formContato').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Obrigado pelo seu contato! Sua mensagem foi enviada.');
+});
